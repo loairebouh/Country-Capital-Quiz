02ef61c6b7b19a9e22277bcc5ba39735826db16a
loairebouh <loairebouh@gmail.com>
2024-06-13 01:12:25 +0100

    v2

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const app = express();
 const port = 3000;
 
 let quiz = [
-  { country: "France", capital: "Paris" },
-  { country: "United Kingdom", capital: "London" },
-  { country: "United States of America", capital: "New York" },
+	{ country: "France", capital: "Paris" },
+	{ country: "United Kingdom", capital: "London" },
+	{ country: "United States of America", capital: "New York" },
 ];
 
 let totalCorrect = 0;
@@ -20,36 +20,36 @@ let currentQuestion = {};
 
 // GET home page
 app.get("/", async (req, res) => {
-  totalCorrect = 0;
-  await nextQuestion();
-  console.log(currentQuestion);
-  res.render("index.ejs", { question: currentQuestion });
+	totalCorrect = 0;
+	await nextQuestion();
+	console.log(currentQuestion);
+	res.render("index.ejs", { question: currentQuestion });
 });
 
 // POST a new post
 app.post("/submit", (req, res) => {
-  let answer = req.body.answer.trim();
-  let isCorrect = false;
-  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
-    totalCorrect++;
-    console.log(totalCorrect);
-    isCorrect = true;
-  }
+	let answer = req.body.answer.trim();
+	let isCorrect = false;
+	if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+		totalCorrect++;
+		console.log(totalCorrect);
+		isCorrect = true;
+	}
 
-  nextQuestion();
-  res.render("index.ejs", {
-    question: currentQuestion,
-    wasCorrect: isCorrect,
-    totalScore: totalCorrect,
-  });
+	nextQuestion();
+	res.render("index.ejs", {
+		question: currentQuestion,
+		wasCorrect: isCorrect,
+		totalScore: totalCorrect,
+	});
 });
 
 async function nextQuestion() {
-  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+	const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
 
-  currentQuestion = randomCountry;
+	currentQuestion = randomCountry;
 }
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+	console.log(`Server is running at http://localhost:${port}`);
 });
